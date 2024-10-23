c5dcf501c64c10ff8706b3597a0e031b2e865484
efl-v2 <gerfedorov0@gmail.ru>
2024-10-24 00:45:53 +0500

    24.10

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,166 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
-
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import java.util.ArrayList;
 import java.util.List;
 
+
 public class UserDaoHibernateImpl implements UserDao {
+
     public UserDaoHibernateImpl() {
-
     }
-
 
     @Override
     public void createUsersTable() {
+        try (Session session = Util.getSessionFactory().openSession()) {
+            Transaction transaction = null;
+            try {
+                transaction = session.beginTransaction();
 
+                String sqlQuery = "CREATE TABLE IF NOT EXISTS users (\n" +
+                        " id BIGINT PRIMARY KEY AUTO_INCREMENT,\n" +
+                        " name VARCHAR(255) NOT NULL,\n" +
+                        " lastName VARCHAR(255) NOT NULL,\n" +
+                        " age TINYINT NOT NULL\n" +
+                        " )";
+
+                session.createSQLQuery(sqlQuery).executeUpdate();
+
+                transaction.commit();
+
+            } catch (Exception e) {
+                if (transaction != null) {
+                    transaction.rollback();
+                }
+                e.printStackTrace();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void dropUsersTable() {
+
+        try (Session session = Util.getSessionFactory().openSession()) {
+            Transaction transaction = null;
+            try {
+                transaction = session.beginTransaction();
+
+                String sqlQuery = "DROP TABLE IF EXISTS users";
+
+                session.createSQLQuery(sqlQuery).executeUpdate();
+
+                transaction.commit();
+
+            } catch (Exception e) {
+                if (transaction != null) {
+                    transaction.rollback();
+                }
+                e.printStackTrace();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
 
+        User user = new User(name, lastName, age);
+
+        try (Session session = Util.getSessionFactory().openSession()) {
+
+            Transaction transaction = null;
+
+            try {
+                transaction = session.beginTransaction();
+
+                session.save(user);
+
+                transaction.commit();
+
+            } catch (Exception e) {
+                if (transaction != null) {
+                    transaction.rollback();
+                }
+                e.printStackTrace();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
 
+        try (Session session = Util.getSessionFactory().openSession()) {
+
+            Transaction transaction = null;
+
+            try {
+                transaction = session.beginTransaction();
+
+                User user = session.get(User.class, id);
+
+                session.delete(user);
+
+                transaction.commit();
+
+            } catch (Exception e) {
+                if (transaction != null) {
+                    transaction.rollback();
+                }
+                e.printStackTrace();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+
+        List<User> users = new ArrayList<>();
+
+        try (Session session = Util.getSessionFactory().openSession()) {
+
+            Query<User> query = session.createQuery("from User", User.class);
+
+            users = query.getResultList();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return users;
     }
 
     @Override
     public void cleanUsersTable() {
+        try (Session session = Util.getSessionFactory().openSession()) {
+            Transaction transaction = null;
+            try {
+                transaction = session.beginTransaction();
 
+                session.createQuery("delete from User").executeUpdate();
+
+                transaction.commit();
+
+            } catch (Exception e) {
+                if (transaction != null) {
+                    transaction.rollback();
+                }
+                e.printStackTrace();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,129 +1,129 @@
-package jm.task.core.jdbc.dao;
-
-import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.util.Util;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
-
-public class UserDaoJDBCImpl implements UserDao {
-
-    public UserDaoJDBCImpl() {
-
-    }
-
-    @Override
-    public void createUsersTable() {
-        // Код для создания таблицы
-        try (Connection connection = Util.getConnection()) {
-            String sql = "CREATE TABLE IF NOT EXISTS users (\n" +
-                    " id BIGINT PRIMARY KEY AUTO_INCREMENT,\n" +
-                    " name VARCHAR(255) NOT NULL,\n" +
-                    " lastName VARCHAR(255) NOT NULL,\n" +
-                    " age TINYINT NOT NULL\n" +
-                    " )";
-            try (Statement statement = connection.createStatement()) {
-                statement.executeUpdate(sql);
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Override
-    public void dropUsersTable() {
-        // Код для удаления таблицы
-        try (Connection connection = Util.getConnection()) {
-            String sql = "DROP TABLE IF EXISTS users";
-            try (Statement statement = connection.createStatement()) {
-                statement.executeUpdate(sql);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void saveUser(String name, String lastName, byte age) {
-        // Код для сохранения пользователя в таблицу
-        Connection connection = Util.getConnection();
-        try {
-            PreparedStatement statement = connection.prepareStatement("INSERT INTO users (name, lastName, age) VALUES (?, ?, ?)");
-            connection.setAutoCommit(false);
-            statement.setString(1, name);
-            statement.setString(2, lastName);
-            statement.setByte(3, age);
-            statement.executeUpdate();
-            connection.commit();
-        } catch (SQLException e) {
-            try {
-                connection.rollback();
-            } catch (SQLException ex) {
-                throw new RuntimeException(ex);
-            }
-        }
-    }
-
-    @Override
-    public void removeUserById(long id) {
-        // Код для удаления пользователя по id из таблицы
-        Connection connection = Util.getConnection();
-        try {
-            connection.setAutoCommit(false);
-            PreparedStatement statement = connection.prepareStatement("DELETE FROM users WHERE id = ?");
-            statement.setLong(1, id);
-            statement.executeUpdate();
-            connection.commit();
-        } catch (SQLException e) {
-            try {
-                connection.rollback();
-            } catch (SQLException ex) {
-                throw new RuntimeException(ex);
-            }
-        }
-    }
-
-    @Override
-    public List<User> getAllUsers() {
-        // Код для получения всех пользователей из таблицы
-        List<User> allUsers = new ArrayList<>();
-        try (Connection connection = Util.getConnection();
-             Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery("SELECT * FROM users")) {
-            while (resultSet.next()) {
-                User user = new User();
-                user.setId(resultSet.getLong("id"));
-                user.setName(resultSet.getString("name"));
-                user.setLastName(resultSet.getString("lastName"));
-                user.setAge(resultSet.getByte("age"));
-                allUsers.add(user);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return allUsers;
-    }
-    @Override
-    public void cleanUsersTable() {
-        // Код для очистки таблицы
-        Connection connection = Util.getConnection();
-        try {
-            connection.setAutoCommit(false);
-            String sql = "DELETE FROM users";
-            try (Statement statement = connection.createStatement()) {
-                statement.executeUpdate(sql);
-            }
-            connection.commit();
-        } catch (SQLException e) {
-            try {
-                connection.rollback();
-            } catch (SQLException ex) {
-                throw new RuntimeException(ex);
-            }
-        }
-    }
-}
+//package jm.task.core.jdbc.dao;
+//
+//import jm.task.core.jdbc.model.User;
+//import jm.task.core.jdbc.util.Util;
+//import java.sql.Connection;
+//import java.sql.PreparedStatement;
+//import java.sql.ResultSet;
+//import java.sql.SQLException;
+//import java.sql.Statement;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class UserDaoJDBCImpl implements UserDao {
+//
+//    public UserDaoJDBCImpl() {
+//
+//    }
+//
+//    @Override
+//    public void createUsersTable() {
+//        // Код для создания таблицы
+//        try (Connection connection = Util.getConnection()) {
+//            String sql = "CREATE TABLE IF NOT EXISTS users (\n" +
+//                    " id BIGINT PRIMARY KEY AUTO_INCREMENT,\n" +
+//                    " name VARCHAR(255) NOT NULL,\n" +
+//                    " lastName VARCHAR(255) NOT NULL,\n" +
+//                    " age TINYINT NOT NULL\n" +
+//                    " )";
+//            try (Statement statement = connection.createStatement()) {
+//                statement.executeUpdate(sql);
+//            }
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//
+//    @Override
+//    public void dropUsersTable() {
+//        // Код для удаления таблицы
+//        try (Connection connection = Util.getConnection()) {
+//            String sql = "DROP TABLE IF EXISTS users";
+//            try (Statement statement = connection.createStatement()) {
+//                statement.executeUpdate(sql);
+//            }
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    @Override
+//    public void saveUser(String name, String lastName, byte age) {
+//        // Код для сохранения пользователя в таблицу
+//        Connection connection = Util.getConnection();
+//        try {
+//            PreparedStatement statement = connection.prepareStatement("INSERT INTO users (name, lastName, age) VALUES (?, ?, ?)");
+//            connection.setAutoCommit(false);
+//            statement.setString(1, name);
+//            statement.setString(2, lastName);
+//            statement.setByte(3, age);
+//            statement.executeUpdate();
+//            connection.commit();
+//        } catch (SQLException e) {
+//            try {
+//                connection.rollback();
+//            } catch (SQLException ex) {
+//                throw new RuntimeException(ex);
+//            }
+//        }
+//    }
+//
+//    @Override
+//    public void removeUserById(long id) {
+//        // Код для удаления пользователя по id из таблицы
+//        Connection connection = Util.getConnection();
+//        try {
+//            connection.setAutoCommit(false);
+//            PreparedStatement statement = connection.prepareStatement("DELETE FROM users WHERE id = ?");
+//            statement.setLong(1, id);
+//            statement.executeUpdate();
+//            connection.commit();
+//        } catch (SQLException e) {
+//            try {
+//                connection.rollback();
+//            } catch (SQLException ex) {
+//                throw new RuntimeException(ex);
+//            }
+//        }
+//    }
+//
+//    @Override
+//    public List<User> getAllUsers() {
+//        // Код для получения всех пользователей из таблицы
+//        List<User> allUsers = new ArrayList<>();
+//        try (Connection connection = Util.getConnection();
+//             Statement statement = connection.createStatement();
+//             ResultSet resultSet = statement.executeQuery("SELECT * FROM users")) {
+//            while (resultSet.next()) {
+//                User user = new User();
+//                user.setId(resultSet.getLong("id"));
+//                user.setName(resultSet.getString("name"));
+//                user.setLastName(resultSet.getString("lastName"));
+//                user.setAge(resultSet.getByte("age"));
+//                allUsers.add(user);
+//            }
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//        return allUsers;
+//    }
+//    @Override
+//    public void cleanUsersTable() {
+//        // Код для очистки таблицы
+//        Connection connection = Util.getConnection();
+//        try {
+//            connection.setAutoCommit(false);
+//            String sql = "DELETE FROM users";
+//            try (Statement statement = connection.createStatement()) {
+//                statement.executeUpdate(sql);
+//            }
+//            connection.commit();
+//        } catch (SQLException e) {
+//            try {
+//                connection.rollback();
+//            } catch (SQLException ex) {
+//                throw new RuntimeException(ex);
+//            }
+//        }
+//    }
+//}
